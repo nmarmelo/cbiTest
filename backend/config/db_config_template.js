43c647f2988cb52c25075a10caabb543330bfad1
Nathan Marmelo <nathanmarmelo@hotmail.com>
2023-10-11 17:57:56 -0400

    add template db_config file

---
+++ backend/config/db_config_template.js
@@ -0,0 +1,14 @@
+module.exports = {
+    HOST: "test3",
+    PORT: "1433",
+    USER: "test",
+    PASSWORD: "test",
+    DB: "test",
+    dialect: "mssql",
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    }
+  };
